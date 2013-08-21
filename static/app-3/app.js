dee8717deb4e57a3b3657baa94758231470574b0
Thomas B Homburg <thomas@homburg.dk>
2013-08-21 15:48:46 +0200

    Added tab example

---
+++ static/app-3/app.js
@@ -0,0 +1,46 @@
+(function () {
+	var app = angular.module("myApp", ["ngResource", "ngAnimate", "ui.bootstrap"]);
+
+	app.factory("Persons", function ($resource) {
+		return $resource("/persons/:id", {"id": "@id"});
+	});
+
+	app.directive("person", function() {
+		return {
+			restrict: "E",
+			scope: {
+				person: "=givenPerson",
+				delete: "&"
+			},
+			templateUrl: "person.html"
+		}
+	});
+
+	app.controller("HelloCtrl", function($scope, Persons) {
+
+		$scope.persons = Persons.query(function () {
+			$scope.activePerson = $scope.persons[0]
+		})
+
+		// Add person and refresh model
+		$scope.addPerson = function () {
+			var p = new Persons($scope.newPerson);
+			p.$save(function (newResource) {
+				$scope.persons.push(newResource)
+			});
+		};
+
+		// Delete person and refresh model
+		$scope.deletePerson = function (index) {
+			$scope.persons[index].$remove(function () {
+				$scope.persons.splice(index, 1)
+			});
+		};
+
+		$scope.ixActivePerson = 0
+			
+		$scope.setActivePerson = function (index) {
+			$scope.ixActivePerson = index
+		}
+	});
+})();
